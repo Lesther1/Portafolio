45e97f57df3ec55f90b3730a096c77240eaaa4fa
Lesther1 <lesthercrespo@gmail.com>
2022-12-17 22:29:55 -0500

    Modificacion de las redes sociales

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Navegationbar from './components/navbar/Navegationbar'
 import Welcome from './components/welcome-section/Welcome'
+import Socialmedias from './components/socialmedias/Socialmedias'
 function App() {
 
   return (
     <div>
       <Navegationbar />
-      <Welcome />
+      <div id="home-section">
+        <Welcome />
+        <div id="container-sm">
+          <Socialmedias />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/socialmedias/Socialmedias.jsx
@@ -0,0 +1,11 @@
+function Socialmedias () {
+    return(
+        <div id="social-container">
+            <img src="./images/linkedin-logo.jpg" alt="" />
+            <img src="./images/discor-logo.png" alt="" />
+            <img src="./images/gihub-icon.png" alt="" id="github" />
+            <img src="./images/linkedin-logo.jpg" alt="" />
+        </div>
+    )
+}
+export default Socialmedias
